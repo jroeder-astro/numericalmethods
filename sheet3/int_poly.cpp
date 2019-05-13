f877305ce4f244f37ee9e3292a816145c6ed3463
badhatharry <jan@roeders-home.de>
2019-05-13 21:05:35 +0200

    neville works, cubic spline to go

--- sheet3/int_poly.cpp
+++ sheet3/int_poly.cpp
@@ -7,6 +7,9 @@ double function1(double x);
 
 double function2(double x);
 
+double neville(double xbar, vector<double> *xvals, vector<double> *yvals, 
+               int ordr, int indx);
+ 
 double bisection(double (*f)(double), double upper, double lower, double eps);
 
 bool criterium(double one, double two);
@@ -14,16 +17,12 @@ bool criterium(double one, double two);
 double linear(vector<double> *xvals, vector<double> *yvals, 
               int n, double xbar);
  
-main(){
-  const int N = 99; 
-  vector<double> xi(100);
-  vector<double> yi(100);
-  double upper =  10;
-  double lower = -10;
-  double root1 = 0.0;
-  double root2 = 0.0;
-  double eps  = 1e-7;
-  int i1 = 0; int i2 = 0;
+int main(){
+  int N = 99; 
+  vector<double> xi(N+1); vector<double> yi(N+1);
+  double upper =  10; double lower = -10;
+  double root1 = 0.0; double root2 = 0.0;
+  double eps  = 1e-7; int i1 = 0; int i2 = 0;
 
   // find roots of given function
   // brackets found by plotting the function
@@ -38,17 +37,48 @@ main(){
 
   // linear interpolation
   // find the points xbar lies between
+/*
   printf("f(-5)[intp] = %3.6f\n", linear(&xi,&yi,N+1,-5.));
   printf("f(-5)[true] = %3.6f\n", function1(-5.));
   printf("f(+5)[intp] = %3.6f\n", linear(&xi,&yi,N+1,+5.));
   printf("f(+5)[true] = %3.6f\n", function1(+5.));
- 
+*/
+
+  // Neville's algorithm for first function
+  upper = 10; lower = -10; double xbar = 5.;
+ // printf("f1(+5)[intp] = %3.8f\n", neville(xbar, &xvals, &yvals, 3, 0));
+ // printf("f1(+5)[true] = %3.8f\n", function2(xbar));
 
 
+  /*......................*/
 
+  // Neville's algorithm for second function
+  // I could have used the same array as above but decided against since
+  // I want to try different N
 
+  N = 999; upper = 20; lower = 0;
+  step = (upper-lower)/N;
+  vector<double> xj(N+1);  vector<double> yj(N+1);
+  vector<double> xvals(4); vector<double> yvals(4);
 
+  for (i1 = 0; i1 < N+1; i1++) xj[i1] = lower + (double)i1*step;   
+  for (i1 = 0; i1 < N+1; i1++) yj[i1] = function2(xj[i1]); 
+  xbar = 12.;
 
+  for (i1 = 0; i1 < N+1; i1++) {
+    if (xj[i1] > xbar) {
+      for (i2 = 0; i2 < 4; i2++) {
+        xvals[i2] = xj[i1-2+i2];            
+        // printf("xval: %3.4f\n", xvals[i2]);
+        yvals[i2] = function2(xj[i1-1+i2]); 
+        // printf("yval: %3.4f\n", yvals[i2]);
+      }
+      break;
+    }
+  }
+
+  printf("f2(+12)[intp] = %3.8f\n", neville(xbar, &xvals, &yvals, 3, 0));
+  printf("f2(+12)[true] = %3.8f\n", function2(xbar));
 
   return 0;
 }
@@ -62,13 +92,23 @@ double function2(double x){
   return cos(M_PI*x*1/4)+1/2*cos(M_PI*x*3/4)-1/2*cos(M_PI*x*1/2);
 }
 
+/*
+P(0,1,2,3): gesuchtes Polynom
+  ^     ^"ordr"
+"indx"
+*/
 
-double neville(double xbar, vector<double> *xvals, 
-               vector<double> *yvals){
-  int i1 = 0; int i2 = 0;
-
-  
-
+double neville(double xbar, vector<double> *xvals, vector<double> *yvals, 
+               int ordr, int indx){
+  if (ordr-indx == 1) {                        //   vvv
+    return ((xbar - (*xvals)[ordr]) * (*yvals)[indx] +
+           ((*xvals)[indx] - xbar) * (*yvals)[ordr]) /
+           ((*xvals)[indx] - (*xvals)[ordr]);
+  } 
+                                                        //                  vvv
+  return ((xbar - (*xvals)[ordr]) * neville(xbar, xvals, yvals, ordr-1, indx)+
+         ((*xvals)[indx] - xbar) * neville(xbar, xvals, yvals, ordr, indx+1))/
+         ((*xvals)[indx] - (*xvals)[ordr]);
 }
 
 
