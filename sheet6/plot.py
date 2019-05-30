9fbc0f886b36ae5a98fc30f5df796980b7c5e07d
badhatharry <jan@roeders-home.de>
2019-05-30 21:23:24 +0200

    sheet6 done

--- sheet6/plot.py
+++ sheet6/plot.py
@@ -7,7 +7,8 @@ phi = []
 r = []
 chi = []
 
-with open('e_023_r1_526.txt', 'r') as csvfile:
+#with open('e_023_r1_526.txt', 'r') as csvfile:
+with open('test_data.txt', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter = ',')
     for row in plots:
         t.append(float(row[0]))
@@ -16,11 +17,13 @@ with open('e_023_r1_526.txt', 'r') as csvfile:
         chi.append(float(row[3]))
       
 #plt.plot(t, r, label='r(t)')
+#plt.plot(t[1], r[1], 'ro')
 #plt.plot(chi, r, label = 'r(chi)')
 #plt.plot(chi, phi, label='phi(chi)')
 #plt.plot(chi, r*np.cos(phi), label='rcos(phi), chi')
 #plt.plot(chi, r*np.sin(phi), label='rsin(phi), chi')
 plt.plot(r*np.cos(phi), r*np.sin(phi), label='rsin(phi), rcos(phi)')
+plt.plot(r[1]*np.cos(phi[1]), r[1]*np.sin(phi[1]),'ro', label='rsin(phi), rcos(phi)')
 plt.legend()
 plt.show()
 
--- sheet6/rk_orbit.cpp
+++ sheet6/rk_orbit.cpp
@@ -15,14 +15,14 @@ double lat(double e, double r1);
 main(){
 
   int N = 2; int i1 = 0; int i2 = 0;
-  int max_steps = 2000;
-  double tau = 0.01;  
+  int max_steps = 1000;
+  double tau = 2.*M_PI/100.;
 
   // remember to put in e and r1 
 
-//  double e = 0.3; double r1 = 7.0;
+  double e = 0.3; double r1 = 7.0;
 //  double e = 0.2; double r1 = 6.1;
-  double e = 0.22722; double r1 = 5.26;
+//  double e = 0.22722; double r1 = 5.26;
  
 
   double y_in[N] = {0.0, 0.0};
@@ -49,6 +49,11 @@ main(){
     y[i1][0], y[i1][1], rad[i1], chi[i1]);  
   }
 
+//  printf("\nDelta(phi)_theo = %3.5lf\n", 6.*M_PI/lat(e, r1));   
+  printf("\nDelta(phi)_theo = %3.5lf\n", 2.*M_PI*(1.+3./lat(e, r1)));   
+//  for (i1 = 101; i1 <= max_steps; i1+=100)
+  printf("Delta(phi)_calc = %3.5lf\n", y[105][1]-y[5][1]);
+
   return 0;
 }
 
@@ -60,14 +65,21 @@ void rk_step(int N, vector<double> *y_chi, double chi,
              double *y_chi_next, double tau,
              double e, double r1){
   int i1 = 0;
+  vector<double> y_tmp(N);
 
   double k1[N]; f_times_tau(y_chi, chi, k1, tau, e, r1);
-
-  vector<double> y_tmp(N); 
+ 
   for (i1 = 0; i1 < N; i1++) y_tmp[i1] = (*y_chi)[i1] + 0.5*k1[i1];
   double k2[N]; f_times_tau(&y_tmp, chi+0.5*tau, k2, tau, e, r1);
-  
-  for (i1 = 0; i1 < N; i1++) y_chi_next[i1] = (*y_chi)[i1] + k2[i1];
+ 
+  for (i1 = 0; i1 < N; i1++) y_tmp[i1] = (*y_chi)[i1] + 0.5*k2[i1];
+  double k3[N]; f_times_tau(&y_tmp, chi+0.5*tau, k3, tau, e, r1);
+
+  for (i1 = 0; i1 < N; i1++) y_tmp[i1] = (*y_chi)[i1] + k3[i1];
+  double k4[N]; f_times_tau(&y_tmp, chi+tau, k4, tau, e, r1);
+
+  for (i1 = 0; i1 < N; i1++) 
+    y_chi_next[i1] = (*y_chi)[i1] + 1/6.*(k1[i1]+2*k2[i1]+2*k3[i1]+k4[i1]);
 }
 
 void f_times_tau(vector<double> *y_chi, double chi, double *ftau, 
