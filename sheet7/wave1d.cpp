65d5ef75890760d0608ec0bcd4cd19eb1e4db61c
badhatharry <jan@roeders-home.de>
2019-06-15 18:03:45 +0200

    ftcs isn't even working with periodic boundaries, error is nowhere to be found

--- sheet7/wave1d.cpp
+++ sheet7/wave1d.cpp
@@ -9,8 +9,8 @@ double u0(double x);
 double u0_x(double x);
 
 void ftcs_tstep_atj(int N, vector<vector<double>> &y_n, 
-                    vector<double> &y_next, double j, 
-                    double tau, double alpha);
+                    vector<double> &y_next, int j, double tau, 
+                    double alpha, int tmax_i);
 
 
 int main() {
@@ -21,30 +21,30 @@ int main() {
   double v = 1.; // factor in wave equation
 
   double xlow = 0.; double xup = 1.; // range for position
-  double tlow = 0.; double tup = 3.; // range for time
+  double tlow = 0.; double tup = 1.; // range for time
 
   vector<vector<vector<double>>> 
-        y(tsteps+1, vector<vector<double>>(xsteps+1, vector<double>(N)));
+        y(tsteps+2, vector<vector<double>>(xsteps+2, vector<double>(N)));
         // y[i][j][k] <=> index n, index j, and r/s/u
-  vector<double> t(tsteps+1); // time array
-  vector<double> x(xsteps+1); // position array
+  vector<double> t(tsteps+2); // time array
+  vector<double> x(xsteps+2); // position array
 
   // fill up time array
-  for (i1 = 0; i1 <= tsteps; i1++) {
+  for (i1 = 0; i1 <= tsteps+1; i1++) {
     t[i1] = i1 * (tup-tlow)/(double)tsteps;
     // printf("%f, %d\n", t[i1], i1);
   }
-  printf("time filled\n");
+  // printf("time filled\n");
 
   // fill up position array and calculate initial values (iv's @ n=0)
-  for (i1 = 0; i1 <= xsteps; i1++) {
+  for (i1 = 0; i1 <= xsteps+1; i1++) {
     x[i1] = i1 * (xup-xlow)/xsteps;
-    // printf("position calc\n");
+    printf("%3.5f\n", x[i1]);
     y[0][i1][0] = v * u0_x(x[i1]);
     y[0][i1][1] = 0.;
     y[0][i1][2] = u0(x[i1]);
   }
-  printf("positions and IV's calculated\n");
+  // printf("positions and IV's calculated\n");
  
   double alpha = v * (tup-tlow)/tsteps * xsteps/(xup-xlow);
   vector<double> y_t(N);
@@ -52,17 +52,22 @@ int main() {
 
   // calculate one time step for every then move to the next time step,
   // again for all x and so on
-  for (i1 = 0; i1 <= tsteps; i1++) {
-    for (i2 = 0; i2 <= xsteps; i2++) {
-      ftcs_tstep_atj(N, y[i1], y_t, x[i2], t_unit, alpha);
+  // for (i1 = 1; i1 <= tsteps+1; i1++) {
+  for (i1 = 1; i1 <= 101; i1++) {
+    for (i2 = 1; i2 <= xsteps+1; i2++) {  // never ever are the indices right
+      
+      // printf("in ftcs loop\n");
+
+      ftcs_tstep_atj(N, y[i1], y_t, i2, t_unit, alpha, tsteps);
       for (i3 = 0; i3 < N; i3++) y[i1+1][i2][i3] = y_t[i3];
     }
   }
 
-
-  // output? boundaries?
-
-
+  // output
+  for (i3 = 1; i3 <= xsteps+1; i1++) {
+    printf("%3.8f,%3.8lf,%3.8f\n", x[i3], y[0][i3][2], y[i1][i3][2]);
+  }
+ 
   return 0;
 }
 
@@ -78,11 +83,15 @@ double u0_x(double x) {
 }
 
 void ftcs_tstep_atj(int N, vector<vector<double>> &y_n, 
-                    vector<double> &y_next, double j, 
-                    double tau, double alpha) {
+                    vector<double> &y_next, int j, double tau, 
+                    double alpha, int tmax_i) {
   // calculates one time step with FTCS at position j
-  y_next[0] = y_n[j][0] + alpha/2. * (y_n[j+1][1] - y_n[j-1][1]);
-  y_next[1] = y_n[j][1] + alpha/2. * (y_n[j+1][0] - y_n[j-1][0]);
+  // periodic boundary conditions (hopefully)
+  int a = j-1; int b = j+1;
+  if (j-1 < 1) a = tmax_i;
+  if (j+1 > tmax_i+1) {b = 1;} //printf("hello there\n");}
+  y_next[0] = y_n[j][0] + alpha/2. * (y_n[b][1] - y_n[a][1]);
+  y_next[1] = y_n[j][1] + alpha/2. * (y_n[b][0] - y_n[a][0]);
   y_next[2] = y_n[j][2] + alpha/2. * tau * y_n[j][1];
 }
 
