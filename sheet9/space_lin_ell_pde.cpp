b197290530ca94a8c0471e74bd2474ee927bcbf8
badhatharry <jan@roeders-home.de>
2019-08-02 10:14:27 +0200

    sheet 9 working, sheetX isn't

--- sheet9/space_lin_ell_pde.cpp
+++ sheet9/space_lin_ell_pde.cpp
@@ -24,11 +24,18 @@ main() {
     vector<vector<vector<double>>> 
         y(Nt, vector<vector<double>>(Ns+2, vector<double>(Ns+2)));
     double y_tmp = 0.0;
+ 
+    // Lnorm: L_inf; LAnorm: L^A_inf; L2norm: L^A_2
+
     double Lnorm = 0.0; double Ltemp = 0.0; double error = 0.000001;
+    vector<double> Lnorms;
+    double LAnorm = 0.0; double LAtemp = 0.0; vector<double> LAnorms;
+    double L2norm = 0.0; vector<double> L2norms;
+
 
     // initial for all timesteps
     // the ghost zones are initialized with the boundary condition
-    // here, boundary means phi = phi_A = nonlin(x,y)
+    // here, boundary means phi = phi_A = -1./5.*nonlin(x,y)
 
     for (i1 = 0; i1 < Nt-1; i1++) {
         for (i2 = 1; i2 < Ns+1; i2++) {
@@ -36,43 +43,70 @@ main() {
                 y[i1][0][i3] = analytic(0, (double)i3*D);
                 y[i1][Ns+1][i3] = analytic(((double)Ns+1.)*D, (double)i3*D);
                 y[i1][i2][i3] = 1.0;
-                // printf("%f\n", y[i1][i2][i3]);
             }
             y[i1][i2][0] = analytic((double)i2*D, 0);
-            // printf("y[%d][%d][%d] = %f\n", i1, i2, 0, y[i1][i2][0]);
             y[i1][i2][Ns+1] = analytic((double)i2*D, ((double)Ns+1.)*D);
         }
     }
 
-    // do-loop Gauss-Seidel
+
+    // for-loop Gauss-Seidel
 
     for (i1 = 1; i1 < Nt-1; i1++) {
-        Lnorm = 0;
+        Lnorm = 0.0; LAnorm = 0.0; L2norm = 0.0;
         for (i2 = 1; i2 < Ns+1; i2++) {
             for (i3 = 1; i3 < Ns+1; i3++) {
                 gauss_seidel(y[i1], &y_tmp, y[i1+1], i2, i3, D);
                 y[i1+1][i2][i3] = y_tmp;
                 
-                Ltemp = y_tmp + sin((double)i2*D+2.*(double)i3*D);
+                Ltemp = fabs(5.*sin((double)i2*D+2.*(double)i3*D) + 
+                    (-4.*y[i1][i2][i3] + (y[i1][i2+1][i3] + y[i1][i2][i3+1]+
+                    y[i1+1][i2-1][i3]+y[i1+1][i2][i3-1])) / (D*D));  
+               
+                LAtemp = y[i1][i2][i3]-sin((double)i2*D+2*(double)i3*D);
+                L2norm += LAtemp*LAtemp;
+
                 if (Ltemp > Lnorm) Lnorm = Ltemp;
+                if (LAtemp > LAnorm) LAnorm = LAtemp;
                 else continue;
             }
         }
+        L2norm /= (Ns*Ns);
+        Lnorms.push_back(Lnorm); 
+        LAnorms.push_back(LAnorm); 
+        L2norms.push_back(L2norm);
 
-        if (Lnorm < error) {
-            printf("Lnorm = %f\n", Lnorm);
-            break;
-        }
+        if (fabs(Lnorm) < error) break;
     } 
+
 
     // print results
 
+    // task a)
     for (i3 = 0; i3 < Ns+2; i3++) {
         for (i2 = 0; i2 < Ns+2; i2++) {
             printf("%3.8lf,%3.8lf,%3.8lf\n", 
                   (double)i3*D, (double)i2*D, y[i1-1][i3][i2]);   
         }
     }
+
+/*
+    // task b)
+    double ratio = 0.0;
+    for (i3 = 1; i3 < Lnorms.size(); i3++) {
+        ratio = Lnorms[i3]/Lnorms[i3-1] - 1.;
+        printf("%d,%lf\n", i3, ratio);
+    }
+*/
+
+/*
+    // task c)
+    for (i3 = 0; i3 < LAnorms.size(); i3++) 
+        printf("%d,%lf\n", i3, LAnorms[i3]);
+    for (i3 = 0; i3 < L2norms.size(); i3++) 
+        printf("%d,%lf\n", i3, L2norms[i3]);
+*/
+
 
     return 0;
 }
